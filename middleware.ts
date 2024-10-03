2890f82fd28fc1b97c7fe6a57b9f773b2a59fe07
armanisadeghi <132974515+armanisadeghi@users.noreply.github.com>
2024-10-02 20:02:07 -0700

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,72 +1,21 @@
 // File: middleware.ts
 
-import { type NextRequest, NextResponse } from 'next/server'
-import { updateSession } from "@/utils/supabase/middleware";
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/utils/supabase/middleware'
 
-export default async function middleware(request: NextRequest) {
-    const path = request.nextUrl.pathname;
-    if (path.startsWith('/auth/callback')) {
-        return NextResponse.next();
-    }
-
-    const shouldUpdateSession = shouldApplyMiddleware(path);
-
-    if (shouldUpdateSession) {
-        const response = await updateSession(request);
-        if (response instanceof NextResponse && response.headers.get("Location")) {
-            const originalUrl = request.nextUrl.clone();
-            const redirectUrl = new URL(response.headers.get("Location")!, request.url);
-
-            originalUrl.searchParams.forEach((value, key) => {
-                redirectUrl.searchParams.set(key, value);
-            });
-
-            return NextResponse.redirect(redirectUrl);
-        }
-
-        return response;
-    } else {
-        return NextResponse.next();
-    }
-}
-
-function shouldApplyMiddleware(path: string): boolean {
-    const excludedPaths = [
-        '/',
-        '/_next/static',
-        '/_next/image',
-        '/favicon.ico',
-        '/site.webmanifest',
-        '/forgot-password',
-        '/reset-password',
-        '/sign-in',
-        '/sign-up',
-        '/test',
-        '/tests',
-        '/testing',
-        '/about',
-        '/contact',
-        '/dash',
-        '/blog',
-        '/pricing',
-        '/auth',
-    ];
-
-    for (const excludedPath of excludedPaths) {
-        if (path === excludedPath) {
-            return false;
-        }
-    }
-
-    if (path.match(/\.(svg|png|jpg|jpeg|gif|webp)$/)) {
-        return false;
-    }
-
-    return true;
+export async function middleware(request: NextRequest) {
+    return await updateSession(request)
 }
 
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon\\.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * Feel free to modify this pattern to include more paths.
+         */
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
-};
+}
