85eb7d2dd3332033876c59e2b2e402526949bf1c
armanisadeghi <132974515+armanisadeghi@users.noreply.github.com>
2024-12-08 18:57:02 -0800

    flashcardsa app working with some serverside integration and a lot of progress, but had to use local data unfortunately with minor link error fixed

--- constants/appSidebarLinks.tsx
+++ constants/appSidebarLinks.tsx
@@ -41,7 +41,7 @@ export const primaryLinks = [
     },
     {
         label: 'Flash Cards',
-        href: '/flashcards',
+        href: '/flashcard',
         icon: (
             <CreditCard className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0"/>
         ),
