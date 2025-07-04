6d4fa988380b22fa3f4f95a21c581dd35e8b64c8
armanisadeghi <132974515+armanisadeghi@users.noreply.github.com>
2025-07-04 12:01:42 -0700

    workflow updates broken for now

--- features/workflows-xyflow/admin/WorkflowAdminOverlay.tsx
+++ features/workflows-xyflow/admin/WorkflowAdminOverlay.tsx
@@ -28,8 +28,8 @@ export const WorkflowAdminOverlay: React.FC<WorkflowAdminOverlayProps> = ({
 }) => {
     // Redux state
     const workflow = useAppSelector((state) => workflowsSelectors.workflowById(state, workflowId));
-    const workflowNodes = useAppSelector((state) => workflowNodesSelectors.nodesByWorkflowId(state, workflowId));
-    const workflowWithNodes = useAppSelector((state) => workflowsSelectors.workflowWithNodesById(state, workflowId));
+    const workflowNodes = useAppSelector((state) => workflowNodesSelectors.nodesByWorkflowId(state)(workflowId));
+    const workflowWithNodes = useAppSelector((state) => workflowsSelectors.workflowWithNodes(state, workflowId));
 
     // React Flow direct state hooks
     const reactFlowNodes = useNodes();
