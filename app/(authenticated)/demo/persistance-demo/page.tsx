3b8df97c923cce0d2c8c3c32b7fd9694e11776fa
armanisadeghi <132974515+armanisadeghi@users.noreply.github.com>
2025-10-22 23:39:37 -0700

    more cleanup

--- app/(authenticated)/demo/persistance-demo/page.tsx
+++
@@ -1,9 +0,0 @@
-import ParentComponent from "./ParentComponent";
-
-export default function Page() {
-    return (
-        <div className="w-full h-full">
-            <ParentComponent />
-        </div>
-    )
-}
