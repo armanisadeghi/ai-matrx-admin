e6a770d68fc7d772e8005e26c6ab0b54b9efaaf1
Arman Sadeghi <arman@armansadeghi.com>
2025-12-08 10:35:50 -0800

    Fix Tailwind v4 darkMode syntax: array to string

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ import { createUtilities } from './utils/tailwind-config/utilities';
 import { plugins } from './utils/tailwind-config/plugins';
 
 const config: Config = {
-    darkMode: ["class"],
+    darkMode: "class",
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
