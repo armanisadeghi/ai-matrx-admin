59eaf52e5d595d161d2f2d1e7d203e9ab6cd7fc2
Armani Sadeghi <arman@armansadeghi.com>
2026-04-25 14:43:16 -0700

    updates to fix the entity legacy issue

--- components/matrx/Entity/prewired-components/layouts/smart-layouts/SmartResizableLayout/SmartResizableLayout.tsx
+++ components/matrx/Entity/prewired-components/layouts/smart-layouts/SmartResizableLayout/SmartResizableLayout.tsx
@@ -1,62 +1,69 @@
-'use client';
-import React from 'react';
-import { NestedResizableLayout, Section } from '@/components/matrx/resizable/NestedResizableLayout';
-import { EntitySelectionSection } from './EntitySelectionSection';
-import { QuickReferenceSection } from './QuickReferenceSection';
-import { EntityDataSection } from './EntityDataSection';
+"use client";
+import React from "react";
+import {
+  NestedResizableLayout,
+  Section,
+} from "@/components/matrx/resizable/NestedResizableLayout";
+import { EntitySelectionSection } from "./EntitySelectionSection";
+import { QuickReferenceSection } from "./QuickReferenceSection";
+import { EntityDataSection } from "./EntityDataSection";
 import { UnifiedLayoutProps } from "@/components/matrx/Entity/prewired-components/layouts/types";
-import { DEFAULT_RESIZABLE_LAYOUT_OPTIONS } from '@/app/(authenticated)/tests/forms/entity-management-smart-fields/configs';
+import { DEFAULT_RESIZABLE_LAYOUT_OPTIONS } from "@/app/entities/layout/configs";
 
+export const SmartResizableLayout: React.FC<UnifiedLayoutProps> = (
+  unifiedLayoutProps,
+) => {
+  const selectedEntity = unifiedLayoutProps.layoutState?.selectedEntity || null;
+  const options =
+    unifiedLayoutProps.resizableLayoutOptions ||
+    DEFAULT_RESIZABLE_LAYOUT_OPTIONS;
 
-export const SmartResizableLayout: React.FC<UnifiedLayoutProps> = (unifiedLayoutProps) => {
-    const selectedEntity = unifiedLayoutProps.layoutState?.selectedEntity || null;
-    const options = unifiedLayoutProps.resizableLayoutOptions || DEFAULT_RESIZABLE_LAYOUT_OPTIONS;
+  const {
+    leftColumnWidth,
+    topLeftHeight,
+    minColumnWidth,
+    minSectionHeight,
+    quickRefCollapsible,
+    leftColumnCollapsible,
+  } = options;
 
-
-    const {
-        leftColumnWidth,
-        topLeftHeight,
-        minColumnWidth,
-        minSectionHeight,
-        quickRefCollapsible,
-        leftColumnCollapsible
-    } = options;
-
-    const sections: Section[] = [
+  const sections: Section[] = [
+    {
+      type: "nested",
+      defaultSize: leftColumnWidth,
+      minSize: minColumnWidth,
+      maxSize: 40,
+      collapsible: leftColumnCollapsible,
+      sections: [
         {
-            type: 'nested',
-            defaultSize: leftColumnWidth,
-            minSize: minColumnWidth,
-            maxSize: 40,
-            collapsible: leftColumnCollapsible,
-            sections: [
-                {
-                    type: 'content',
-                    content: <EntitySelectionSection {...unifiedLayoutProps} />,
-                    defaultSize: topLeftHeight,
-                    minSize: minSectionHeight,
-                    maxSize: 30,
-                    collapsible: false
-                },
-                {
-                    type: 'content',
-                    content: selectedEntity && <QuickReferenceSection {...unifiedLayoutProps} />,
-                    defaultSize: 100 - topLeftHeight,
-                    minSize: minSectionHeight,
-                    collapsible: quickRefCollapsible
-                }
-            ]
+          type: "content",
+          content: <EntitySelectionSection {...unifiedLayoutProps} />,
+          defaultSize: topLeftHeight,
+          minSize: minSectionHeight,
+          maxSize: 30,
+          collapsible: false,
         },
         {
-            type: 'content',
-            content: selectedEntity && <EntityDataSection {...unifiedLayoutProps} />,
-            defaultSize: 100 - leftColumnWidth,
-            minSize: minColumnWidth,
-            collapsible: false
-        }
-    ];
+          type: "content",
+          content: selectedEntity && (
+            <QuickReferenceSection {...unifiedLayoutProps} />
+          ),
+          defaultSize: 100 - topLeftHeight,
+          minSize: minSectionHeight,
+          collapsible: quickRefCollapsible,
+        },
+      ],
+    },
+    {
+      type: "content",
+      content: selectedEntity && <EntityDataSection {...unifiedLayoutProps} />,
+      defaultSize: 100 - leftColumnWidth,
+      minSize: minColumnWidth,
+      collapsible: false,
+    },
+  ];
 
-    return <NestedResizableLayout sections={sections} />;
+  return <NestedResizableLayout sections={sections} />;
 };
 
 export default SmartResizableLayout;
